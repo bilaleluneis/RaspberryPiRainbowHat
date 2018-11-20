0f14823ca99cc3393385244d5d8065f05ef2fcbb
pi <bilaleluneis@gmail.com>
2018-11-20 03:12:52 +0000

    Jieshu Wang's initial impl of Rainbow Shield

--- generic_rainbowshield_JS.py
+++ generic_rainbowshield_JS.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 __author__ = "Jieshu Wang"
 __since__ = "November 2018"
 __email__ = "foundwonder@gmail.com"
@@ -17,22 +19,48 @@ colors_dic: Dict[str, Tuple[int, int, int]] = {
     "violet": (139, 0, 255),
     "white": (255, 255, 255),
     "none": (0, 0, 0)
-    # "random": (rd.randint(0, 255), rd.randint(0, 255), rd.randint(0, 255))
 }
 
 
-class Color(ABC):
-    def __init__(self, color_name: str):
-        self.__color_name = color_name
+class Color(object):
+    def __init__(self, color_name: str= None, color_rgb: Tuple[int, int, int]= None):
+        if color_name is None and color_rgb is None:
+            raise Exception("must at least pass one value for either name or rgb")
+        elif color_name is not None:
+            self.__color_name = color_name
+        elif color_rgb is not None:
+            self.__rgb_value: Tuple[int, int, int] = color_rgb
 
-    # fixme: you can make the bellow a function at top level of file and remove the need for the Color class
-    def get_rgb(self) -> Tuple[int, int, int]:
+    @property
+    # @abstractmethod
+    def rgb(self) -> Tuple[int, int, int]:
         if self.__color_name in colors_dic:  # todo: the way you checked for key in the Dict is so cool, keep it!
             return colors_dic[self.__color_name]
-        elif self.__color_name == "random":
+        elif self.__rgb_value is None and self.__color_name == "random":
             return rd.randint(0, 255), rd.randint(0, 255), rd.randint(0, 255)
+        elif self.__color_name is None and self.__rgb_value is not None:
+            return self.__rgb_value
         else:
-            raise Exception("unknown color name!")
+            raise Exception("unknown color!")
+
+    def set_rgb(self, new_rgb: Tuple[int, int, int]):
+        if new_rgb in colors_dic.values():
+            self.__color_name = list(colors_dic.keys())[list(colors_dic.values()).index(new_rgb)]
+        else:
+            raise Exception("unknown rgb!")
+
+    def get_name(self):
+        return self.__color_name
+
+    def __iadd__(self, new_color: str) -> Color:  # colorA += colorB
+        r1, g1, b1 = self.rgb
+        r2, g2, b2 = colors_dic[new_color]
+        r3 = int((r2 - r1) / 10)
+        g3 = int((g2 - g1) / 10)
+        b3 = int((b2 - b1) / 10)
+        self.__rgb_value = (r1 + r3), (g1 + g3), (b1 + b3)
+        self.__color_name = None
+        return self
 
 
 class Timer(ABC):
@@ -44,13 +72,20 @@ class Timer(ABC):
 
 
 class Led(Color, Timer):
-    def __init__(self, led_index: int, led_color: str, led_timer: float):
-        Color.__init__(self, led_color)  # fixme: do you really need Color class? you are using Tuple in the dict
+
+    @property
+    def rgb(self) -> Tuple[int, int, int]:
+        r, g, b = super().rgb
+        print("calling the overridden get_rgb in Led class with values R={} G={} B={} !".format(r, g, b))
+        return r, g, b
+
+    def __init__(self, led_index: int, led_timer: float, led_color: str=None, led_rgb: Tuple[int, int, int]=None):
+        Color.__init__(self, color_name=led_color, color_rgb=led_rgb)
         Timer.__init__(self, led_timer)
         self.__led_index = led_index
 
     def on(self):
-        r, g, b = self.get_rgb()
+        r, g, b = self.rgb
         rh.rainbow.set_pixel(self.__led_index, r, g, b)
         rh.rainbow.show()
 
@@ -92,7 +127,7 @@ class RainbowShield:
         self.__time_pattern = time_pattern
         self.__leds: List[Led] = []  # always initialize before use even to empty
         for i in range(7):
-            self.__leds.append(Led(i, self.__color_pattern, self.__time_pattern))
+            self.__leds.append(Led(i, self.__time_pattern, self.__color_pattern))
 
     def rainbow_run(self):
         for i in range(7):
@@ -103,8 +138,19 @@ class RainbowShield:
 
 def main():
     # Led(1, "red", 1.0).on()
+    # Color("red")
+    # while True:
+    # RainbowShield("random", "random", 0.3).rainbow_run()
+    # Led(1, "green", 2).set_rgb((255, 0, 0))
+    led1 = Led(1, 0.2, "blue", None)
     while True:
-        RainbowShield("random", "random", 0.3).rainbow_run()
+        led1.blink()
+        rgb = led1.rgb
+        led_color = Color(color_name=None, color_rgb=rgb)
+        led_color += "red"
+        led_rgb = led_color.rgb
+        led2 = Led(1, 0.2, None, led_rgb)
+        led2.blink()
 
 
 if __name__ == "__main__":
